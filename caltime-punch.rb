a2ce9ef406acee9fe5b5d91903a629d371648ccc
Nicholas Herson <nicholas.herson@gmail.com>
2015-04-22 21:08:19 -0700

    add check for bad credentials; help text

--- caltime-punch.rb
+++ caltime-punch.rb
@@ -3,21 +3,43 @@
 require 'mechanize'
 require 'highline/import'
 
+if ["help", "-h", "--help"].include?(ARGV[0])
+  puts "
+    USAGE: caltime-punch.rb
+    No arguments needed, the script will prompt for CalNet ID and passphrase
+
+    Source: https://github.com/nherson/caltime-punch
+    "
+  exit 1
+end
+
+# Initialize Mechanize agent and set config
 agent = Mechanize.new
 agent.follow_meta_refresh = true
+
 caltime_timestamp_url = "https://caltimeprod.berkeley.edu/wfc/applications/wtk/html/ess/timestamp-record.jsp"
 caltime_base_url = "https://caltimeprod.berkeley.edu/"
-username = ask("Username:") 
-password = ask("Password:") { |q| q.echo = false }
+cal_sso_url = "https://caltimeprod.berkeley.edu/wfc/logonESS_SSO"
 
-cas_page = agent.get(caltime_base_url)
+page = agent.get(caltime_base_url)
   
-login_result_page = cas_page.form_with(:id => "loginForm") do |form|
-  form.field_with(:id => 'username').value = username
-  form.field_with(:id => 'password').value = password
-end.submit
+loop do
 
-login_result_page.form_with(:action => "https://caltimeprod.berkeley.edu/wfc/logonESS_SSO").submit
+  page = page.form_with(:id => "loginForm") do |form|
+    form.field_with(:id => 'username').value = ask("CalNet Username: ") 
+    form.field_with(:id => 'password').value = ask("CalNet Password: ") { |q| q.echo = false }
+  end.submit
+
+  if page.uri.to_s =~ /auth\.berkeley\.edu/
+    puts ""
+    puts "Login incorrect, please try again."
+  else
+    break
+  end
+
+end
+
+page.form_with(:action => cal_sso_url).submit
 
 # Now post to the page that records a timestamp
 agent.post(caltime_timestamp_url)
